d849f63af0ffeaf2a437f2d7e7efddf8158b087c
PhamHuuThuan <92664025+PhamHuuThuan@users.noreply.github.com>
2023-05-09 19:32:29 +0700

    read data from todos and orderlist used fetch

--- Js/script.js
+++ Js/script.js
@@ -74,3 +74,61 @@ switchMode.addEventListener('change', function () {
 		document.body.classList.remove('dark');
 	}
 })
+
+//get du lieu todos tu api len giao dien
+const linkToDos = '/Json/todos.json';
+fetch(linkToDos)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(todos){
+        var todoList = document.querySelector('.todo-list');
+        for(let i=0; i< todos.length;i++){
+            var todo = todos[i];
+            var li = document.createElement('li');
+            li.className = todo.status ? 'completed' : 'not-completed';
+            var p = document.createElement('p');
+            p.textContent = todo.todolist;
+            li.appendChild(p);
+            var iTag = document.createElement('i');
+            iTag.className = 'bx bx-dots-vertical-rounded';
+            li.appendChild(iTag);
+            todoList.appendChild(li);
+        }
+    });
+
+//get du lieu order tu file orderlist
+const linkOrderList = '/Json/orderList.json';
+fetch(linkOrderList)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(orderList){
+        var tableBody = document.querySelector('tbody');
+        for(let i=0; i< orderList.length;i++){
+            var order = orderList[i];
+            var tr = document.createElement('tr');
+            
+            var tdUser = document.createElement('td');
+            var img = document.createElement('img');
+            img.src = order.linkIMG;
+            tdUser.appendChild(img);
+            var p = document.createElement('p');
+            p.textContent = order.user;
+            tdUser.appendChild(p);
+            tr.appendChild(tdUser);
+            
+            var tdDate = document.createElement('td');
+            tdDate.textContent = order.dateorder;
+            tr.appendChild(tdDate);
+            
+            var tdStatus = document.createElement('td');
+            var span = document.createElement('span');
+            span.className = 'status ' + order.status.toLowerCase();
+            span.textContent = order.status;
+            tdStatus.appendChild(span);
+            tr.appendChild(tdStatus);
+            
+            tableBody.appendChild(tr);
+        }
+    });
